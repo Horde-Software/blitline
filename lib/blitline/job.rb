6bd46b590ee93ba69b743377ba765c9b7aec7a98
Erich L. Timkar <erich@hordesoftware.com>
2012-07-07 18:46:59 -0500

    addding src_type support

--- lib/blitline/job.rb
+++ lib/blitline/job.rb
@@ -1,8 +1,8 @@
 class Blitline
   class Job
     include AttributeJsonizer
-    attr_accessor :application_id, :src, :postback_url, :functions
-
+    attr_accessor :application_id, :src, :postback_url, :functions, :src_type
+    SRC_TYPE_MULTI_PAGE = 'multi_page'
     def initialize(image_src, application_id = nil)
       @src = image_src
       @functions = []
@@ -18,6 +18,7 @@ class Blitline
     def validate
       raise "Job must have an application_id" if @application_id.nil?
       raise "Job must have an image_src to work on" if @src.nil?
+      raise "Vadid values for src_type are '#{SRC_TYPE_MULTI_PAGE}', nil or empty." unless @src_type.nil? || @src_type.empty? || @src_type == 'multi_page'
       @functions.each { |f| f.validate }
     end
   end
